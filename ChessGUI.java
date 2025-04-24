8a9843dcba6d5bedcdde1baac19aed8f1af168da
JoshSkyner <joshskyner06@gmail.com>
2025-04-24 21:09:55 +0100

    New

--- ChessGUI.java
+++ ChessGUI.java
@@ -11,18 +11,19 @@ public class ChessGUI {
 
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-
-
+        GridLayout layout = new GridLayout(8,8);
+        frame.add(panel);
+        frame.setLayout(layout);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,1000);
         frame.setLocationRelativeTo(null);
-        frame.setLayout(new GridLayout());
         
 
         frame.setVisible(true);
 
 
             
+        
         
 
     }
