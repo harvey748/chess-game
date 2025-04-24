93c07a2b816ffb6716cae83fc4e0aca8733d85b0
JoshSkyner <joshskyner06@gmail.com>
2025-04-24 20:00:58 +0100

    Frame Creation

--- ChessGUI.java
+++ ChessGUI.java
@@ -1,6 +1,29 @@
 import java.awt.*;
-
+import javax.swing.*;
 
 public class ChessGUI {
     
+
+
+
+
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,1000);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+
+            
+        
+        
+
+    }
+
+
 }
