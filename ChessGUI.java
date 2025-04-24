3ddca14defdd6c978e3025e57843685ac6baba83
harvey748 <strettonh@coventry.ac.uk>
2025-04-25 00:16:45 +0100

    adding board squares (test)

--- ChessGUI.java
+++ ChessGUI.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 
 public class ChessGUI {
     
-    private ImagePanel bluePanel = new ImagePanel("Blue.png");
-    private ImagePanel whitePanel = new ImagePanel("White.png");
+    private static ImagePanel bluePanel = new ImagePanel("Blue.png");
+    private static ImagePanel whitePanel = new ImagePanel("White.png");
 
 
 
@@ -12,21 +12,43 @@ public class ChessGUI {
 
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        
         GridLayout layout = new GridLayout(8,8);
+
         frame.add(panel);
         frame.setLayout(layout);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,1000);
         frame.setLocationRelativeTo(null);
+        
+        //putting the colour in the board
+        for(int i=0; i<=7; i++){
+            for(int j=0; j<=7; j++){
 
-        for (int i=0; i<=7; int j=0; j<=7; i++; j++){
+                if(i%2==0){
+                    
+                    for(int n=0; n<8; n++){
+                        if(n%2==0){
+                            panel.add(new JLabel(bluePanel));
+                        }
+                        else{
+                            panel.add(new JLabel(whitePanel));
+                        }
+                    }
 
-            if (i=0 && j=0){
-
-                Square_Blue = new ImageIcon("Blue"+ i + ".png");
+                }
+                else{
+                    for(int m=0; m<8; m++){
+                        if(m%2==0){
+                            panel.add(new JLabel(whitePanel));
+                        }
+                        else{
+                            panel.add(new JLabel(bluePanel));
+                        }
+                    }
+                }
 
             }
+
         }
         
 
