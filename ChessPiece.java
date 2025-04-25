bdc1999ed3c27168bedb3f052586799840ae73d5
harvey748 <strettonh@coventry.ac.uk>
2025-04-25 10:13:42 +0100

    added subclasses for chess piece

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,6 +1,36 @@
 import java.awt.*;
 
-
+//superclass
 public class ChessPiece {
+    
+}
+
+//subclass pawn
+class Pawn extends ChessPiece{
+
+}
+
+//subclass rook
+class Rook extends ChessPiece{
+
+}
+
+//subclass bishop
+class Bishop extends ChessPiece{
+
+}
+
+//subclass knight 
+class Knight extends ChessPiece{
+
+}
+
+//subclass queen
+class Queen extends ChessPiece{
+
+}
+
+//subclass king
+class King extends ChessPiece{
     
 }
