f790abbfc0627f22c49bcf919b1455492c75b249
JoshSkyner <joshskyner06@gmail.com>
2025-04-25 12:49:45 +0100

    Fixing Subclasses To separate
    Everything in one file needs to be inside the main class. Thats just how it works in Java so when making new classes they have to be separate. I didnt extend them yet tho

--- ChessPiece.java
+++ ChessPiece.java
@@ -5,32 +5,3 @@ public class ChessPiece {
     
 }
 
-//subclass pawn
-class Pawn extends ChessPiece{
-
-}
-
-//subclass rook
-class Rook extends ChessPiece{
-
-}
-
-//subclass bishop
-class Bishop extends ChessPiece{
-
-}
-
-//subclass knight 
-class Knight extends ChessPiece{
-
-}
-
-//subclass queen
-class Queen extends ChessPiece{
-
-}
-
-//subclass king
-class King extends ChessPiece{
-    
-}
