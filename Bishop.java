69fa083dc4efc429fb3d38c0004df704b1aadc01
harvey748 <strettonh@coventry.ac.uk>
2025-04-29 01:26:48 +0100

    all pieces now move the way they should and can take

--- Bishop.java
+++ Bishop.java
@@ -9,15 +9,62 @@ public class Bishop extends ChessPiece{
     }
     @Override
     public boolean isValidMove(JButton fromButton, JButton toButton){
-        ChessPiece.replaceChessPiece(toButton, fromButton);
-       
-        JButton temp = new JButton();
-        temp = this.location;
-        this.location = toButton;
-        fromButton = temp;
-        return true;
         
-        //Harveys gay
-    }
+        int To_xPos = -1;
+        int To_yPos = -1;
+        int From_xPos = -1;
+        int From_yPos = -1;
+        for(int i =0; i<8; i++){
+            for(int j = 0; j<8; j++){
+                if(boardSquares[i][j] == toButton){
+                    To_yPos = i;
+                    To_xPos = j;
+                }
+            }
+        }
+
+        for(int i =0; i<8; i++){
+            for(int j = 0; j<8; j++){
+                if(boardSquares[i][j] == fromButton){
+                    From_yPos = i;
+                    From_xPos = j;
+                }
+            }
+        }
+        if((Math.abs(To_yPos - From_yPos) == Math.abs(To_xPos - From_xPos))){
+
+        
+            
+            int startY = Math.min(From_yPos, To_yPos) + 1;
+            int endY = Math.max(From_yPos, To_yPos);
+            int startX = Math.min(From_xPos, To_xPos) + 1;
+            int endX = Math.max(From_xPos, To_xPos);
+            for(int y = startY, x = startX; y < endY && x < endX; y++, x++){
+                
+                if(boardSquares[y][x].getIcon() != null){
+                    return false; 
+                }
+            }
+            
+        
+        if(boardSquares[To_yPos][To_xPos].getIcon() == null){
     
+            ChessPiece.moveChessPiece(toButton, fromButton);
+        }
+        else{
+            ChessPiece.replaceChessPiece(toButton, fromButton);
+        }
+        return true;
+    }
+      
+            
+    return false;
+    }
+
+      
+
+
 }
+    
+    
+
--- Queen.java
+++ Queen.java
@@ -10,13 +10,90 @@ public class Queen extends ChessPiece {
     }
     @Override
     public boolean isValidMove(JButton fromButton, JButton toButton){
-        ChessPiece.replaceChessPiece(toButton, fromButton);
+        int To_xPos = -1;
+        int To_yPos = -1;
+        int From_xPos = -1;
+        int From_yPos = -1;
+        for(int i =0; i<8; i++){
+            for(int j = 0; j<8; j++){
+                if(boardSquares[i][j] == toButton){
+                    To_yPos = i;
+                    To_xPos = j;
+                }
+            }
+        }
 
-        JButton temp = new JButton();
-        temp = this.location;
-        this.location = toButton;
-        fromButton = temp;
+        for(int i =0; i<8; i++){
+            for(int j = 0; j<8; j++){
+                if(boardSquares[i][j] == fromButton){
+                    From_yPos = i;
+                    From_xPos = j;
+                }
+            }
+        }
+        if((Math.abs(To_yPos - From_yPos) == Math.abs(To_xPos - From_xPos))){
+
+        
+            
+            int startY = Math.min(From_yPos, To_yPos) + 1;
+            int endY = Math.max(From_yPos, To_yPos);
+            int startX = Math.min(From_xPos, To_xPos) + 1;
+            int endX = Math.max(From_xPos, To_xPos);
+            for(int y = startY, x = startX; y < endY && x < endX; y++, x++){
+                
+                if(boardSquares[y][x].getIcon() != null){
+                    return false; 
+                }
+            }
+            
+        
+        if(boardSquares[To_yPos][To_xPos].getIcon() == null){
+    
+            ChessPiece.moveChessPiece(toButton, fromButton);
+        }
+        else{
+            ChessPiece.replaceChessPiece(toButton, fromButton);
+        }
         return true;
+    }
+
+    if((To_yPos - From_yPos <= 8 || To_yPos - From_yPos >= -8) && (To_xPos == From_xPos) || (To_xPos - From_xPos <= 8 || To_xPos - From_xPos >= -8) && (From_yPos == To_yPos)){
+
+        
+        if(From_xPos == To_xPos){
+            int start = Math.min(From_yPos, To_yPos) + 1;
+            int end = Math.max(From_yPos, To_yPos);
+            for(int y = start; y < end; y++){
+                if(boardSquares[y][From_xPos].getIcon() != null){
+                    return false; 
+                }
+                else{
+                    
+                }
+            }
+        }
+        if(From_yPos == To_yPos){
+            int start = Math.min(From_xPos, To_xPos) + 1;
+            int end = Math.max(From_xPos, To_xPos);
+            for(int x = start; x < end; x++){
+                if(boardSquares[From_yPos][x].getIcon() != null){
+                    return false; 
+                }
+            }
+
+        } 
+    
+    if(boardSquares[To_yPos][To_xPos].getIcon() == null){
+
+        ChessPiece.moveChessPiece(toButton, fromButton);
+    }
+    else{
+        ChessPiece.replaceChessPiece(toButton, fromButton);
+    }
+    return true;
+    } 
+        
+    return false;
         
     }
 }
